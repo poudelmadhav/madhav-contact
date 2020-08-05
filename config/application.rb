1987d2dc437908c8fca2320384c90ec39f354ef3
Madhav Paudel <poudelmadhav143@gmail.com>
2020-08-05 23:12:36 +0545

    rack cors enabled to allow cors header origin

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,12 @@ module MadhavContact
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    config.middleware.insert_before ActionDispatch::Static, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :options]
+      end
+    end
   end
 end
