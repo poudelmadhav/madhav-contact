60b63a543aadf3cc6e09ed87a29b6aa2b8eb387d
Madhav Paudel <poudelmadhav143@gmail.com>
2020-12-16 23:54:15 +0545

    static home page added

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  get '/_health' => 'application#health_check'
+
   namespace :api do
     namespace :v1 do
       resources :contacts, only: :create
     end
   end
 
-  get '/_health' => 'application#health_check'
+  root 'home#show'
+
+  resource :home, only: :show, controller: :home
 end
